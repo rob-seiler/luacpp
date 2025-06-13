418f7abce1f5f6b353ae23234a3afc453d567269
Robert <101367646+rob-seiler@users.noreply.github.com>
2025-06-13 22:11:24 +0200

    Refactor operator traits to separate header (#10)

--- include/luacpp/Metatable.hpp
+++ include/luacpp/Metatable.hpp
@@ -7,49 +7,117 @@
 #include <type_traits>
 #include <typeinfo>
 #include <utility>
+#include "OperatorTraits.hpp"
 
 namespace Lua {
 
-template <typename, typename = void>
-struct has_add_operator : std::false_type { };
-
-template <typename T>
-struct has_add_operator<T, std::void_t<decltype(std::declval<T>() + std::declval<T>())>> : std::true_type { };
-
-template <typename, typename = void>
-struct has_sub_operator : std::false_type { };
-
-template <typename T>
-struct has_sub_operator<T, std::void_t<decltype(std::declval<T>() - std::declval<T>())>> : std::true_type { };
 
 namespace detail {
-	template <typename T>
-	void registerDefaultMetatable(State& state) {
-		state.createMetaTable(Metatable<T>::metatableName(), [](Table& mt) {
-			if constexpr (has_add_operator<T>::value) {
-				int (*addFunc)(lua_State*) = [](lua_State* lvm) -> int {
-					State L(lvm);
-					T* lhs = L.getArgument<T*>(1);
-					T* rhs = L.getArgument<T*>(2);
-					T result = *lhs + *rhs;
-					Metatable<T>::create(L, result);
-					return 1;
-				};
-				mt.setElement(State::MetaTable::Addition, addFunc);
-			}
-			if constexpr (has_sub_operator<T>::value) {
-				int (*subFunc)(lua_State*) = [](lua_State* lvm) -> int {
-					State L(lvm);
-					T* lhs = L.getArgument<T*>(1);
-					T* rhs = L.getArgument<T*>(2);
-					T result = *lhs - *rhs;
-					Metatable<T>::create(L, result);
-					return 1;
-				};
-				mt.setElement(State::MetaTable::Substraction, subFunc);
-			}
-		});
-	}
+        template <typename T>
+        void registerAdd(Table& mt) {
+                if constexpr (has_add_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* lhs = L.getArgument<T*>(1);
+                                T* rhs = L.getArgument<T*>(2);
+                                T result = *lhs + *rhs;
+                                Metatable<T>::create(L, result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::Addition, func);
+                }
+        }
+
+        template <typename T>
+        void registerSub(Table& mt) {
+                if constexpr (has_sub_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* lhs = L.getArgument<T*>(1);
+                                T* rhs = L.getArgument<T*>(2);
+                                T result = *lhs - *rhs;
+                                Metatable<T>::create(L, result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::Substraction, func);
+                }
+        }
+
+        template <typename T>
+        void registerMul(Table& mt) {
+                if constexpr (has_mul_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* lhs = L.getArgument<T*>(1);
+                                T* rhs = L.getArgument<T*>(2);
+                                T result = *lhs * *rhs;
+                                Metatable<T>::create(L, result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::Multiplication, func);
+                }
+        }
+
+        template <typename T>
+        void registerDiv(Table& mt) {
+                if constexpr (has_div_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* lhs = L.getArgument<T*>(1);
+                                T* rhs = L.getArgument<T*>(2);
+                                T result = *lhs / *rhs;
+                                Metatable<T>::create(L, result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::Division, func);
+                }
+        }
+
+        template <typename T>
+        void registerUnaryMinus(Table& mt) {
+                if constexpr (has_unary_minus_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* obj = L.getArgument<T*>(1);
+                                T result = -(*obj);
+                                Metatable<T>::create(L, result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::UnaryMinus, func);
+                }
+        }
+
+        template <typename T>
+        void registerEqual(Table& mt) {
+                if constexpr (has_eq_operator<T>::value) {
+                        int (*func)(lua_State*) = [](lua_State* lvm) -> int {
+                                State L(lvm);
+                                T* lhs = L.getArgument<T*>(1);
+                                T* rhs = L.getArgument<T*>(2);
+                                bool result = *lhs == *rhs;
+                                L.pushToStack(result);
+                                return 1;
+                        };
+                        mt.setElement(State::MetaTable::Equal, func);
+                }
+        }
+
+        template <typename T>
+        void registerOperators(Table& mt) {
+                registerAdd<T>(mt);
+                registerSub<T>(mt);
+                registerMul<T>(mt);
+                registerDiv<T>(mt);
+                registerUnaryMinus<T>(mt);
+                registerEqual<T>(mt);
+        }
+
+        template <typename T>
+        void registerDefaultMetatable(State& state) {
+                state.createMetaTable(Metatable<T>::metatableName(), [](Table& mt) {
+                        registerOperators<T>(mt);
+                });
+        }
 } // namespace detail
 
 /**
